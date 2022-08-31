9fd053bcccbf77287122d901d35e088680f58cf1
Minh Hung <hungthegentle1704@gmail.com>
2022-08-31 20:40:22 +0700

    Da hinh va Truu tuong

--- src/oop/abstraction/PizzaCheese.java
+++ src/oop/abstraction/PizzaCheese.java
@@ -1,4 +1,27 @@
 package oop.abstraction;
 
-public class PizzaCheese {
+public class PizzaCheese extends Pizza {
+    public PizzaCheese(String name) {
+        super(name);
+    }
+
+    @Override
+    void prepare() {
+        System.out.print("Pizza Cheese Prepare ");
+    }
+
+    @Override
+    void sauce() {
+        System.out.print("Pizza Cheese Sauce ");
+    }
+
+    @Override
+    void bake() {
+        System.out.print("Pizza Cheese Bake ");
+    }
+
+    @Override
+    void cut() {
+        System.out.print("Pizza Cheese Cut ");
+    }
 }
--- src/oop/abstraction/PizzaVegetable.java
+++ src/oop/abstraction/PizzaVegetable.java
@@ -1,4 +1,27 @@
 package oop.abstraction;
 
-public class PizzaVegetable {
+public class PizzaVegetable extends Pizza {
+    public PizzaVegetable(String name) {
+        super(name);
+    }
+
+    @Override
+    void prepare() {
+         System.out.print("Pizza Vegetable Prepare");
+    }
+
+    @Override
+    void sauce() {
+        System.out.print("Pizza Vegetable Sauce ");
+    }
+
+    @Override
+    void bake() {
+        System.out.print("Pizza Vegetable Bake ");
+    }
+
+    @Override
+    void cut() {
+        System.out.print("Pizza Vegetable Cut ");
+    }
 }
