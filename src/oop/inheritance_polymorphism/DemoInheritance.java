02ceb6cb230bb835f0d8b6314e076b3b86dd06c8
Minh Hung <hungthegentle1704@gmail.com>
2022-08-31 20:31:31 +0700

    Da hinh va truu tuong

---
+++ src/oop/inheritance_polymorphism/DemoInheritance.java
@@ -0,0 +1,8 @@
+package oop.inheritance_polymorphism;
+
+public class DemoInheritance {
+    public static void main(String[] args) {
+        VietcomBank vietcombank = new VietcomBank();
+        vietcombank.calculateRate(6);
+    }
+}
